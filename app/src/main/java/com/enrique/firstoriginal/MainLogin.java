fc292beedff0f929985996e5ba9447e7a0201bd8
Enrique <fpemlktz@gmail.com>
2021-11-22 12:36:53 +0100

    Material_login_glide

---
+++ app/src/main/java/com/enrique/firstoriginal/MainLogin.java
@@ -0,0 +1,42 @@
+package com.enrique.firstoriginal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class MainLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ImageView mGirl = findViewById(R.id.imageView2);
+
+        Glide.with(this)
+              //.load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+
+                //.centerCrop()
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+                //.circleCrop()
+                .into(mGirl);
+    }
+    public void openMain(View v) {
+        Intent intent = new Intent(MainLogin.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    /*public void openSignup(View v) {
+        Intent intent = new Intent(MainLogin.this, Signup.class);
+        startActivity(intent);
+    }*/
+}
