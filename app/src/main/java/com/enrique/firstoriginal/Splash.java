fd857ec6f5d7f89118789fbea4d10af34bcac22f
Enrique <fpemlktz@gmail.com>
2021-11-28 17:17:30 +0100

    Algunos cambios e investigación version 1

--- app/src/main/java/com/enrique/firstoriginal/Splash.java
+++ app/src/main/java/com/enrique/firstoriginal/Splash.java
@@ -1,16 +1,14 @@
 package com.enrique.firstoriginal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -34,7 +32,7 @@ public class Splash extends AppCompatActivity {
 
         Glide.with(this)
                 //.load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
-                .load(R.drawable.mancuerna)
+                .load(R.drawable.splashimage)
                 .transition(DrawableTransitionOptions.withCrossFade(10))
                 .centerCrop()
 //                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
